ea5d89d1907d56aac1d91076f8e5c82d068ba569
sooryaprabhath <sooryaprabhath.poojary@niveussolutions.com>
2023-06-02 12:29:23 +0530

    port change

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/random-sentence", (req, res) => {
 });
 
 // Start the server
-const port = 3001;
+const port = 9000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
